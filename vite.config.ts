aeff8c0c6cba03e183d4dcb812eb1d9403668d52
nyoupaneroshan <35588093+nyoupaneroshan@users.noreply.github.com>
2025-04-06 23:03:35 +0545

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
@@ -9,6 +8,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
+      external: ['/src/main.tsx'],
       input: {
         main: '/index.html',
       },
